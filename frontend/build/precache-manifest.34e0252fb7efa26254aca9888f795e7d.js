269502fdbeebccee0e68678da416dfe9da132f44
Ido Hershkovits <hershido@gmail.com>
2020-07-14 11:08:39 +0300

    update

---
+++ frontend/build/precache-manifest.34e0252fb7efa26254aca9888f795e7d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17bbc8a0574f76919360a7d2e45754d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6193b79e7737b8c7b300",
+    "url": "/static/css/main.a96d54de.chunk.css"
+  },
+  {
+    "revision": "06756c002949ba4bb907",
+    "url": "/static/js/2.e235d0b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e235d0b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6193b79e7737b8c7b300",
+    "url": "/static/js/main.8edc3071.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  },
+  {
+    "revision": "d08191f13b7ec217fa1556d44b7e3a25",
+    "url": "/static/media/alarm_clock.d08191f1.ttf"
+  },
+  {
+    "revision": "533b96be436a4b61d8da30e44dbf1572",
+    "url": "/static/media/computer-cup-desk-drink-434337 (1).533b96be.jpg"
+  },
+  {
+    "revision": "623c7118249e082fe87a78e08506cb4b",
+    "url": "/static/media/segoeui.623c7118.ttf"
+  }
+]);
