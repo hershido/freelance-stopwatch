b1d8a5fe778de2eb29b268a96661f6e1a34b6a5d
Ido Hershkovits <hershido@gmail.com>
2020-07-14 11:52:21 +0300

    rename

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
 const cors = require('cors');
-const taskRoutes = require('./routes/tasks');
-const userRoutes = require('./routes/users');
+const taskRoutes = require('./approutes/tasks');
+const userRoutes = require('./approutes/users');
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 
